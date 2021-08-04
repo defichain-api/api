abc793462b7b0745f6ee175fcb079a748ea15798
Adrian <adrian@c-m-d.info>
2021-08-04 21:26:49 +0200

    - add RpcRequest Object - install ray package (local debug)

--- app/BlockchainData/Client/Rpc/RpcClientServiceProvider.php
+++ app/BlockchainData/Client/Rpc/RpcClientServiceProvider.php
@@ -6,15 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class RpcClientServiceProvider extends ServiceProvider
 {
-	public function register()
-	{
-		$this->app->singleton(RpcClient::class, fn($app) => new RpcClient(
-		    new RpcClientConfig(
-		        config('rpc.user'),
+    public function register()
+    {
+        $this->app->singleton(RpcClient::class, fn ($app) => new RpcClient(
+            new RpcClientConfig(
+                config('rpc.user'),
                 config('rpc.password'),
                 config('rpc.uri'),
                 config('rpc.port')
             )
         ));
-	}
+    }
 }
